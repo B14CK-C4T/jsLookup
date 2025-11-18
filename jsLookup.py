55ce945b7c0dfde3bcf36c4b1c762b91978a4c95
MusthafaMohammed <musthafamuhammad491@gmail.com>
2025-11-18 21:00:15 +0530

    fixed: file,patterns not found error when using globally

--- jsLookup.py
+++ jsLookup.py
@@ -1,25 +1,46 @@
 import argparse
 import re
+import os
 
-def pattern(pattern_file):
-    patterns = []
-    with open(pattern_file, 'r') as file:
-        for lines in file:
-            patterns.append(lines.strip())
-    return patterns
+#def pattern(pattern_file):
+patterns = ["/static/js/chunk.js","/main.chunk.js","/_next/static/cunks/","/config.js","/env.js","/sw.js","/service-worker.js","main.js.map","/_/firebase/init.js","/intercom.js","/livechat.js","/analytics.js",".mjs""/cdn-cgi/","/cloudflare-assets/","/assets/js/","/vendor.js","/app.js","/bundle.js"]
+  #  try:
+   #     with open(pattern_file, 'r') as file:
+   #         for lines in file:
+   #             patterns.append(lines.strip())
+   #     return patterns
+   # except FileNotFoundError:
+   #     print("[!] Pattern file are missing")
 
-def lookup(file_name, patterns):
-    with open(file_name, 'r') as file:      
-        lines = file.readlines()
-        for pat in patterns:
-            for line in lines:
-                if re.search(pat, line):
-                    print(f"js Pattern: {pat}\tLine: {line.strip()}\n")
+def lookup(file_name, patterns,output_file=None, verbose=None):
+    results = []
+    working_dir = os.getcwd()
+    try:
+        with open(f"{working_dir}{file_name}", 'r') as file:      
+            lines = file.readlines()
+            for pat in patterns:
+                for line in lines:
+                    if re.search(pat, line):
+                        if verbose:
+                            result = f"[#] pattern: {pat} :\t{line.strip()}\n"
+                            results.append(result)
+                        else:
+                            result = f"{line.strip()}\n"
+                            results.append(result)
+
+        if output_file:
+            with open(output_file, 'w') as out:
+                out.writelines(results)
+        else:
+            for result in results:
+                print(result, end='')
+    except FileNotFoundError as e:
+        print(f"[!] {e}")
 
 
 def main():
 
-    pattern_file = 'pattern.txt'
+    #pattern_file = 'pattern.txt'
 
     parser = argparse.ArgumentParser(description="jsParser")
     parser.add_argument('--file','-f',type=str,help='file name',required=True,)
